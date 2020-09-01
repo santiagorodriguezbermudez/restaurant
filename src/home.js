969c9fc9df70a4c49934310d42c68d5cabfd1ac6
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-09-01 12:25:31 -0500

    Implemented home first js module

---
+++ src/home.js
@@ -0,0 +1,21 @@
+
+const home = (() => {
+  const main = document.querySelector('#content');
+  const hero = document.createElement('div');
+  const heroImg = document.createElement('img');
+  heroImg.src = '../src/images/hero_img.svg';
+  const heroTitle = document.createElement('h1');
+  heroTitle.innerHTML = 'Hero Delivery';
+  const heroCopy = document.createElement('h3');
+  heroCopy.innerHTML = "We'll always deliver food on time with the highest quality standards";
+  const heroCta = document.createElement('button');
+  heroCta.innerHTML = 'Order now';
+
+  main.appendChild(hero);
+  hero.appendChild(heroImg);
+  hero.appendChild(heroTitle);
+  hero.appendChild(heroCopy);
+  hero.appendChild(heroCta);
+})();
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('hello world!');
+import home from './home';
+
+home();
